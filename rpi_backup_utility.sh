1a6ca930d0fb28c3e287a80f56e1eebba50530c7
barisalby <baris.albayrak@outlook.com.tr>
2024-06-02 02:29:25 +0300

    fix(NEXT): Storage mount bugs fixed.

--- rpi_backup_utility.sh
+++ rpi_backup_utility.sh
@@ -1,9 +1,72 @@
 #!/bin/bash
+
 SYSTEM="/dev/mmcblk0"
 HARD_DRIVE_TEMP="/tmp/ARG0-HDSWAP"
 BACKUP_DIR="/mnt/mass/"
 BACKUP_COUNT=5
 EXPECTED_COUNT=2
+
+# ANSI COLORS
+YELLOW='\e[33m'
+GREEN='\e[32m'
+WHITE='\e[1;37m'
+RED='\e[1;31m'
+CYAN='\e[1;36m'
+RESET='\e[0m'
+
+checkHARD_DRIVE() {
+    if [ -f "$HARD_DRIVE_TEMP" ]; then
+        HARD_DRIVE=$(cat "$HARD_DRIVE_TEMP")
+        if ! mount | grep -q "$HARD_DRIVE" && lsblk -o NAME | grep -q "$HARD_DRIVE"; then
+            listDevices
+            echo "${YELLOW}${HARD_DRIVE} DEVICE DISCONNECTED${RESET}"
+            promptForDrive
+        fi
+    else
+        listDevices
+        promptForDrive
+    fi
+}
+
+listDevices() {
+    echo ""
+    lsblk --output NAME,TYPE | awk '
+    NR == 1 {print $0}
+    NR > 1 {
+        if ($2 == "part") {
+            printf "\033[1;32m%s\033[0m\n", $0;
+        } else if ($1 ~ /^(sda|sdb|sdc)$/) {
+            printf "\033[1;33m%s\033[0m\n", $0;
+        } else if ($1 ~ /^nvme/) {
+            printf "\033[1;35m%s\033[0m\n", $0;
+        } else {
+            printf "\033[2m%s\033[0m\n", $0;
+        }
+    }'
+    echo ""
+}
+
+promptForDrive() {
+    echo "${WHITE}ENTER HARD DRIVE MOUNT POINT: ${CYAN}(ex: /dev/sda1):${RESET} "
+    read INPUT_DRIVE
+    HARD_DRIVE=$(echo "$INPUT_DRIVE" | sed 's:/*$::')
+    echo "$HARD_DRIVE" > "$HARD_DRIVE_TEMP"
+    mountFunction
+}
+
+checkImages() {
+    echo ""
+    cd "$BACKUP_DIR" || exit
+
+    FILE_COUNT=$(ls -t | wc -l)
+    if [ "$FILE_COUNT" -gt "$BACKUP_COUNT" ]; then
+        FILES_TO_DEL=$(ls -t | tail -n +"$EXPECTED_COUNT")
+        for FILE in $FILES_TO_DEL; do
+            echo "${WHITE}$FILE ${GREEN}~ ${RED}OLD BACKUP FILE HAS BEEN REMOVED FROM ${CYAN}$BACKUP_DIR${RESET}"
+            rm -f "$FILE"
+        done
+    fi
+}
 
 createImage() {
     YYYY=$(date +%Y)
@@ -20,23 +83,25 @@ createImage() {
 
     echo ""
     echo "-------------------------------------------------------------"
-    echo "\e[1;37mRaspberry Pi Backup Utility \e[1;32m~ \e[1;37mhttps://barisalbayrak.net/\e[0m"
+    echo "${WHITE}Raspberry Pi Backup Utility ${GREEN}~ ${WHITE}https://barisalbayrak.net/${RESET}"
     echo "-------------------------------------------------------------"
     dd if=$SYSTEM bs=128K status=progress | gzip > ${BACKUP_DIR}${IMAGE_NAME}
-} 
+
+    latestImage
+}
 
 ejectHARD_DRIVE() {
     echo ""
-    umount -l $HARD_DRIVE
+    umount -l "$HARD_DRIVE"
 }
 
 startBackup() {
     clear
-echo "\e[1;32m⠀⢀⣠⣤⣶⣶⣶⣤⣄⠀⠀⣀⣤⣶⣶⣶⣤⣄⡀⠀
+    echo "${GREEN}⠀⢀⣠⣤⣶⣶⣶⣤⣄⠀⠀⣀⣤⣶⣶⣶⣤⣄⡀⠀
 ⠀⢸⣿⠁⠀⠀⠀⠀⠙⢷⡾⠋⠀⠀⠀⠀⠈⣿⡇⠀
 ⠀⠘⢿⡆⠀⠀⠀⠢⣄⣼⣧⣠⠔⠀⠀⠀⢰⡿⠃⠀
-⠀⠀⠈⠻⣧⣤⣀⣤⣾⣿⣿⣷⣤⣀⣤⣼⠟⠁⠀⠀
-\e[1;31m⠀⠀⣰⡾⠋⠉⣩⣟⠁⠀⠀⠈⣻⣍⠉⠙⢷⣆⠀⠀
+⠀⠀⠈⠻⣧⣤⣀⣤⣾⣿⣿⣷⣤⣀⣤⣼⠟⠁⠀⠀${RED}
+⠀⠀⣰⡾⠋⠉⣩⣟⠁⠀⠀⠈⣻⣍⠉⠙⢷⣆⠀⠀
 ⠀⢀⣿⣀⣤⡾⠛⠛⠷⣶⣶⠾⠛⠛⢷⣤⣀⣿⡀⠀
 ⣰⡟⠉⣿⡏⠀⠀⠀⠀⢹⡏⠀⠀⠀⠀⢹⣿⠉⢻⣆
 ⣿⡇⠀⣿⣇⠀⠀⠀⣠⣿⣿⣄⠀⠀⠀⣸⣿⠀⢸⣿
@@ -44,15 +109,19 @@ echo "\e[1;32m⠀⢀⣠⣤⣶⣶⣶⣤⣄⠀⠀⣀⣤⣶⣶⣶⣤⣄⡀⠀
 ⠀⢸⣿⠀⠀⠈⢿⡇⠀⠀⠀⠀⢸⡿⠁⠀⠀⣿⡇⠀
 ⠀⠀⠻⣧⣀⣀⣸⣿⣶⣤⣤⣶⣿⣇⣀⣀⣼⠟⠀⠀
 ⠀⠀⠀⠈⠛⢿⣿⣿⡀⠀⠀⢀⣿⣿⡿⠛⠁⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠙⠻⠿⠿⠟⠋⠀⠀⠀⠀⠀⠀⠀\e[0m
+⠀⠀⠀⠀⠀⠀⠀⠙⠻⠿⠿⠟⠋⠀⠀⠀⠀⠀⠀⠀${RESET}
 "
-    echo "\e[0;37mStarting backup process on \e[1;32m~ \e[1;36m$HARD_DRIVE\e[0m "
-    echo "\e[0;37mThis process can take up to several hours depending on the system size."
-    echo "\e[1;31mDON'T WRITE ANY DATA TO THE SYSTEM AND DON'T PULL OFF BACKUP STORAGE DURING THIS TIME!\e[0m"
+    echo "${WHITE}Starting backup process on ${GREEN}~ ${CYAN}$HARD_DRIVE${RESET}"
+    echo "${WHITE}This process can take up to several hours depending on the system size."
+    echo "${RED}DON'T WRITE ANY DATA TO THE SYSTEM AND DON'T PULL OFF BACKUP STORAGE DURING THIS TIME!${RESET}"
+    if [ -d "$BACKUP_DIR" ]; then
+        checkImages
+    fi
+
     createImage
     sleep 3
-    checkImages
 }
+
 latestImage() {
     clear
     cd "$BACKUP_DIR" || exit
@@ -63,89 +132,43 @@ latestImage() {
     NEW_DATE=$(date -d "$FILE_MODIFICATION_DATE" "+%Y-%m-%d %H:%M")
 
     if [ -z "$FILESIZE_MB" ]; then
-        echo "\e[1;36mLATEST BACKUP FILE:\n\e[1;37mFile: \e[1;31mFAILED \e[1;37m| Size: \e[1;31m0 MB \e[1;37m|"
+        echo "${CYAN}LATEST BACKUP FILE:\n${WHITE}File: ${RED}NO FILE  ${WHITE}| Size: ${RED}0 MB ${WHITE}|"
     else
-        echo "\e[1;36mLATEST BACKUP FILE:\n\e[1;37mFile: \e[1;32m$FILENAME \e[1;37m| Size: \e[1;32m$FILESIZE_MB MB \e[1;37m| Modification Date: \e[1;32m$NEW_DATE\e[0m"
+        echo "${CYAN}LATEST BACKUP FILE:\n${WHITE}File: ${GREEN}$FILENAME ${WHITE}| Size: ${GREEN}$FILESIZE_MB MB ${WHITE}| Modification Date: ${GREEN}$NEW_DATE${RESET}"
     fi
 
     ejectHARD_DRIVE
 }
 
-listDevices() {
-    echo ""
-
-    lsblk --output NAME,TYPE | awk '
-    NR == 1 {print $0}
-    NR > 1 {
-        if (NR > 1 && $2 == "part") {
-            printf "\033[1;32m%s\033[0m\n", $0;
-        } else if ($1 ~ /^(sda|sdb|sdc)$/) {
-            printf "\033[1;33m%s\033[0m\n", $0;
-        } else if ($1 ~ /^nvme/) {
-            printf "\033[1;35m%s\033[0m\n", $0;
-        } else {
-            printf "\033[2m%s\033[0m\n", $0;
-        }
-    }'
-
-    echo ""
-}
-
-checkHARD_DRIVE() {
-    if [ -f "$HARD_DRIVE_TEMP" ]; then
-        HARD_DRIVE=$(cat "$HARD_DRIVE_TEMP")
-        if ! mount | grep -q "$HARD_DRIVE" && lsblk -o NAME | grep -q "$HARD_DRIVE"; then
-            listDevices
-            echo "$(cat "$HARD_DRIVE_TEMP") DEVICE DISCONNECTED"
-            echo "\e[1;37mENTER HARD DRIVE MOUNT POINT: \e[1;32m~ \e[1;36m(ex: /dev/sda1):\e[0m "
-            read INPUT_DRIVE
-            HARD_DRIVE=$(echo "$INPUT_DRIVE" | sed 's:/*$::')
-            echo "$HARD_DRIVE" > "$HARD_DRIVE_TEMP"
-        fi
-    else
-        listDevices
-        echo "\e[1;37mENTER HARD DRIVE MOUNT POINT: \e[1;32m~ \e[1;36m(ex: /dev/sda1):\e[0m "
-        read INPUT_DRIVE
-        HARD_DRIVE=$(echo "$INPUT_DRIVE" | sed 's:/*$::')
-        echo "$HARD_DRIVE" > "$HARD_DRIVE_TEMP"
-    fi
-}
-
-checkHARD_DRIVE
-
-checkImages() {
-    echo ""
-    cd "$BACKUP_DIR" || exit
-
-    FILE_COUNT=$(ls -t | wc -l)
-    if [ "$FILE_COUNT" -gt "$BACKUP_COUNT" ]; then
-        FILES_TO_DEL=$(ls -t | tail -n +"$EXPECTED_COUNT")
-        for FILE in $FILES_TO_DEL; do
-            echo "\e[1;37m$FILE \e[1;32m~ \e[1;31mOLD BACKUP FILE HAS BEEN REMOVED FROM \e[1;36m$BACKUP_DIR\e[0m"
-            rm -f "$FILE"
-        done
-    fi
-
-    latestImage
-}
-
 mountFunction() {
     if mountpoint -q "$BACKUP_DIR"; then
-        echo "\e[1;36m$HARD_DRIVE\e[0m \e[1;37malready mounted on >>> \e[1;33m$BACKUP_DIR\e[0m";
+        echo "${CYAN}$HARD_DRIVE${RESET} ${WHITE}already mounted on >>> ${YELLOW}$BACKUP_DIR${RESET}";
         startBackup
     else
-        echo "mounting \e[1;36m$HARD_DRIVE\e[0m\n\e[1;31mDON'T PULL OFF YOUR STORAGE DEVICE...\e[0m";
+        echo "mounting ${CYAN}$HARD_DRIVE${RESET}\n${RED}DON'T PULL OFF YOUR STORAGE DEVICE...${RESET}";
         mount "$HARD_DRIVE" "$BACKUP_DIR" 2>/dev/null
         sleep 3
-        echo "\e[1;36m$HARD_DRIVE\e[0m \e[1;37mmounted on >>> \e[1;33m$BACKUP_DIR\e[0m";
+        echo "${CYAN}$HARD_DRIVE${RESET} ${WHITE}mounted on >>> ${YELLOW}$BACKUP_DIR${RESET}";
         startBackup
     fi
 }
+
+if [ -f "$HARD_DRIVE_TEMP" ]; then
+    HARD_DRIVE=$(cat "$HARD_DRIVE_TEMP")
+fi
 
 if [ -d "$BACKUP_DIR" ]; then
     mountFunction
-else
-    mkdir "$BACKUP_DIR" >/dev/null 2>&1
     sleep 2
-    mountFunction
+elif [ ! -d "$BACKUP_DIR" ]; then
+    mkdir -p "$BACKUP_DIR" >/dev/null 2>&1
+    if [ -f "$HARD_DRIVE_TEMP" ]; then
+        echo "${GREEN}BACKUP DIRECTORY CREATED${RESET}"
+        sleep 2
+        mountFunction
+    else
+        echo "${GREEN}BACKUP DIRECTORY CREATED${RESET}"
+        sleep 1
+        checkHARD_DRIVE
+    fi
 fi
